7aa90fc4e4973e5da3fc7fe267e2baa4199d976b
JonHalloran <angmar139@hotmail.com>
2018-07-28 15:59:58 -0700

    Basic user schema

--- models/User.js
+++ models/User.js
@@ -3,7 +3,8 @@ const { Schema } = mongoose;
 
 const userSchema = new Schema({
   googleId: String,
-  displayName: String
+  displayName: String,
+  shoppingCart: Object,
 });
 
 mongoose.model('User', userSchema);
