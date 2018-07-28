939fa91b2ff8a22ca92421ef4972aa7206ae52d9
nwelchr <nwelchr@gmail.com>
2018-07-28 16:35:29 -0700

    Add semicolons

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cookieSession = require('cookie-session')
-const passport = require('passport')
-const bodyParser = require('body-parser')
-const keys = require('./config/keys')
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const keys = require('./config/keys');
 
-require('./models/User')
-require('./models/Nonprofit')
-require('./models/Blog')
-require('./services/passport')
+require('./models/User');
+require('./models/Nonprofit');
+require('./models/Blog');
+require('./services/passport');
 
-mongoose.Promise = global.Promise
+mongoose.Promise = global.Promise;
 mongoose.connect(
   keys.mongoURI,
   { useMongoClient: true }
-)
+);
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
     keys: [keys.cookieKey]
   })
-)
-app.use(passport.initialize())
-app.use(passport.session())
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-require('./routes/authRoutes')(app)
-require('./routes/blogRoutes')(app)
+require('./routes/authRoutes')(app);
+require('./routes/blogRoutes')(app);
 
 if (['production'].includes(process.env.NODE_ENV)) {
-  app.use(express.static('client/build'))
+  app.use(express.static('client/build'));
 
-  const path = require('path')
+  const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve('client', 'build', 'index.html'))
-  })
+    res.sendFile(path.resolve('client', 'build', 'index.html'));
+  });
 }
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Listening on port`, PORT)
-})
+  console.log(`Listening on port`, PORT);
+});
--- models/Nonprofit.js
+++ models/Nonprofit.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose')
-const { Schema } = mongoose
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
 const nonprofitSchema = new Schema({
   googleId: String,
   displayName: String,
   neededItems: { type: Schema.Types.ObjectId, ref: 'Item' }
-})
+});
 
-mongoose.model('Nonprofit', nonprofitSchema)
+mongoose.model('Nonprofit', nonprofitSchema);
