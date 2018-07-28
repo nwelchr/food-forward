d4923ba84e0f18c077d4f0e7d22725ece1e147d9
nwelchr <nwelchr@gmail.com>
2018-07-28 15:31:49 -0700

    First commit

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+  mongoURI:
+    'mongodb://team-aa-hackathon:password1@ds237409.mlab.com:37409/team-aa-hackathon',
+  cookieKey: '123123123'
+}
