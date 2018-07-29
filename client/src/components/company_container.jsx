c5c6ffe89a530c68cdd288eff480d85ad271c291
jiangtt18 <tjiang90111@gmail.com>
2018-07-28 17:42:08 -0700

    basic framework for company component

---
+++ client/src/components/company_container.jsx
@@ -0,0 +1,18 @@
+import { connect } from 'react-redux';
+import { fetchItems, updateItem } from '../../actions/castle_actions';
+
+import Company from './company';
+
+const mapStateToProps = state => ({
+  items: state.items,
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetchItems: () => dispatch(fetchItems()),
+  updateItem: (item) => dispatch(updateItem())
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Company);
