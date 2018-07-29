ad916a9864a06753033e61b5728f059c362c0020
JonHalloran <angmar139@hotmail.com>
2018-07-29 07:23:19 -0700

    Fix shopping screen

--- client/src/components/buyer/Buyer.jsx
+++ client/src/components/buyer/Buyer.jsx
@@ -5,29 +5,64 @@ import CheckoutModal from "./CheckoutModal";
 class Buyer extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            checkout: false
+        };
+
+        this.toggleCheckout = this
+            .toggleCheckout
+            .bind(this);
     }
 
     componentDidMount() {
-        if (this.props.user)
-            this.props.fetchCart(this.props.user._id);
+        if (this.props.user) {
+            this
+                .props
+                .fetchCart(this.props.user._id);
         }
+    }
+
+    toggleCheckout() {
+        console.log('toggle');
+        this.setState({
+            checkout: Boolean(!this.state.checkout)
+        });
+    }
 
     render() {
+        const renderModal = this.state.checkout
+            ? <CheckoutModal/>
+            : '';
 
+        const name = this.props.user
+            ? this.props.user.displayName
+            : '';
+
+        const cartCount = Object
+            .keys(this.props.cart)
+            .length;
+
+        const icon = cartCount > 0
+            ? (
+                <div className="cart-icon-circle">
+                    <div className="cart-icon-number">{cartCount}</div>
+                </div>
+            )
+            : '';
         // let theCheckout = useremail – comapny donating to – shoping cart item
         return (
             <div>
                 <div className="buyer-nav">
-                    <div className="user-profile">User Email</div>
-                    <div className="cart-icon">
-                        <div className="cart-icon-circle">
-                            <div className="cart-icon-number">2</div>
-                        </div>
+                    <div className="user-profile">{name}</div>
+                    <div onClick={this.toggleCheckout} className="cart-icon">
+                        {icon}
                     </div>
                     <div className="blur-filter"/>
                 </div>
+                {renderModal
+}
                 <BuyerIndexContainer/>
-                <CheckoutModal/>
             </div>
         );
     }
