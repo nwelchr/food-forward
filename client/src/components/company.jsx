f12ebbc8ef5d47ec580abe7feab0af635fc8bd18
tjshiu <tiffany.shiu.2@gmail.com>
2018-07-28 18:04:51 -0700

    added cloudinary

--- client/src/components/company.jsx
+++ client/src/components/company.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
 import itemsIndexComponent from './itemIndex';
+const CLOUDINARY_UPLOAD_PRESET = "zfgcp1tk";
+const CLOUDINARY_UPLOAD_URL =
+  "https://api.cloudinary.com/v1_1/dwanjkcku/upload";
+
 
 class Company extends React.Component {
   constructor(props){
@@ -8,6 +12,8 @@ class Company extends React.Component {
     this.state = {
       name: '',
       quota: '',
+      image_url: '', 
+      item_cost: ''
     };
   }
 
@@ -21,10 +27,39 @@ class Company extends React.Component {
   }
 
   update(field) {
-  return (e) => this.setState({
-    [field]: e.currentTarget.value
-  });
-}
+    return (e) => this.setState({
+      [field]: e.currentTarget.value
+    });
+  }
+
+  onImageDrop(files) {
+    this.setState({
+      uploadedFile: files[0]
+    });
+
+    this.handleImageUpload(files[0]);
+  }
+
+  handleImageUpload(file) {
+    let upload = request
+      .post(CLOUDINARY_UPLOAD_URL)
+      .field("upload_preset", CLOUDINARY_UPLOAD_PRESET)
+      .field("file", file);
+
+    upload.end((err, response) => {
+      if (err) {
+        console.error(err);
+      }
+
+      if (response.body.secure_url !== "") {
+        this.setState({
+          image_url: response.body.secure_url
+        });
+      }
+    });
+  }
+
+
 
   render(){
     if (Object.keys(this.props.items).length === 0) {
@@ -34,10 +69,39 @@ class Company extends React.Component {
    }
    const featuredItems = Object.values(this.props.items);
     return(
-     <div>
+      <div>
        <nav>
          <div> Company Name </div>
          <div>  + </div>
+      </nav>
+         <div className="product-contianer">
+          <div className="product-image-upload">
+            <h1>Photo:</h1>
+            <Dropzone
+              multiple={false}
+              accept="image/*"
+              onDrop={this.onImageDrop.bind(this)}
+            >
+              <div className="drop-zone-instructions">
+                <div>Add a Primary Photo</div>
+                <div>Drop an image or click to select a file to upload.</div>
+                <br />
+                <i className="fa fa-camera" aria-hidden="true" />
+              </div>
+            </Dropzone>
+            <div>
+              {this.state.image_url === "" ? null : (
+                <div className="image-upload">
+                  <div>
+                    <img src={this.state.image_url} />
+                  </div>
+                  {this.props.formType === "Create A Product" ? (
+                    <div>{this.state.uploadedFile.name}</div>
+                  ) : null}
+                </div>
+              )}
+            </div>
+          </div>
          <form className='itemForm' onSubmit = {this.handleSubmit}>
            <div className='ItemErros'>
              Errors
@@ -61,9 +125,8 @@ class Company extends React.Component {
 
            <input type='submit'/>
          </form>
-      </nav>
 
-      <itemsIndexComponent />
+        <itemsIndexComponent />
 
      </div>
    );
