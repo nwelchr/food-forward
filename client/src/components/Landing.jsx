7bbc6129098f97fd88ffcb36c1fe9ed16c1e4ac4
Michael Pompili <michael@churroboros.com>
2018-07-28 22:39:59 -0700

    google image scraper code in company.util

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -28,15 +28,22 @@ class Landing extends React.Component {
             <div>
                 <div className="landing">
                     <h1 className="landing-title">Food Forward</h1>
-                    <h3 className="landing-blurp">Give back, get less taxes</h3>
-                    <button onClick={() => this.showmodal()} id="mdl-btn" className="landing-cta">Give Back</button>
+                    <h3 className="landing-blurp">
+                        Give back, get less taxes
+                    </h3>
+                    <button onClick={() => this.showmodal()} id="mdl-btn" className="landing-cta">
+                        Give Back
+                    </button>
                 </div>
                 <div className="modal-content">
-                    <span onClick={() => this.closemodal()} className="close">&times;</span>
-                    <p>Sign in with Google</p>
+                    <div className="sign-in-text">
+                        <p>Sign in with Google</p>
+                        <span onClick={() => this.closemodal()} className="close">&times;</span>
+                    </div> 
+                    
+                    <a href={"/auth/google"}><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/200px-Google_%22G%22_Logo.svg.png"/></a>
                 </div>
-                <div onClick={() => this.closemodal()} id="myModal" className="modal">
-                </div>
+                <div onClick={() => this.closemodal()} id="myModal" className="modal"></div>
             </div>
         );
     }
